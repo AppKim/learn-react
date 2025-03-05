dec445974501ed1d7e2e7fa5c62e1664de59d06d
AppKim <jskim2301@yahoo.co.jp>
2025-03-05 16:08:45 +0900

    add: data fetch

--- next.js/onebite-next/app-router-version/src/app/(with-searchbar)/page.tsx
+++ next.js/onebite-next/app-router-version/src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,53 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
+import { BookData } from "@/types";
+
+async function AllBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+  if (!response.ok) {
+    return <div>occurred error</div>;
+  }
+  const allBooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+async function RecommendBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+  );
+  if (!response.ok) {
+    return <div>occurred error</div>;
+  }
+  const recommendBooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {recommendBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>Recommend</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecommendBooks />
       </section>
       <section>
         <h3>All</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   );
