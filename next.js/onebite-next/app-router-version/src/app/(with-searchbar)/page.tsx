19710fb9c8942b9511726c805f0cf61da2c2e3cd
AppKim <jskim2301@yahoo.co.jp>
2025-03-05 15:02:52 +0900

    add: ui

--- next.js/onebite-next/app-router-version/src/app/(with-searchbar)/page.tsx
+++ next.js/onebite-next/app-router-version/src/app/(with-searchbar)/page.tsx
@@ -1,15 +1,22 @@
-import { useEffect } from "react";
-import styles from "./page.module.css";
-import ClientComponent from "./client-component";
-import ServerComponent from "./server-component";
+import BookItem from "@/components/book-item";
+import style from "./page.module.css";
+import books from "@/mock/books.json";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      index page
-      <ClientComponent>
-        <ServerComponent />
-      </ClientComponent>
+    <div className={style.container}>
+      <section>
+        <h3>Recommend</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>All</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
     </div>
   );
 }
